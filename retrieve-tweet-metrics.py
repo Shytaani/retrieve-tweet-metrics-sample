d6ab61158e34cb5fdfa9e2f6c2daec2f98e8d483
Tanishy <mambaout134@gmail.com>
2022-04-08 02:32:14 +0900

    initial commit

---
+++ retrieve-tweet-metrics.py
@@ -0,0 +1,44 @@
+from requests_oauthlib import oauth1_auth
+import settings
+import requests
+import http
+import csv
+
+
+def json_to_csv(json, headers, row):
+    for k, v in json.items():
+        if type(v) is dict:
+            json_to_csv(v, headers, row)
+        else:
+            if k not in headers:
+                headers.append(k)
+            row[k] = v
+    return headers, row
+
+
+params = {
+    'query': f'from:{settings.user_name}',
+    'tweet.fields': settings.tweets_fields
+}
+
+headers_oauth1 = oauth1_auth.OAuth1(settings.consumer_key, settings.consumer_key_secret, settings.access_token,
+                                    settings.access_token_secret)
+
+response = requests.get(settings.url, params, auth=headers_oauth1)
+
+if response.status_code != http.HTTPStatus.OK:
+    raise Exception(response.status_code, response.text)
+else:
+    result = response.json()['data']
+    csv_headers = ['id', 'text']
+    csv_rows = []
+    for record in result:
+        csv_row = {}
+        csv_headers, csv_row = json_to_csv(record, csv_headers, csv_row)
+        csv_rows.append(csv_row)
+
+    with open('out/sample.csv', 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=csv_headers)
+        writer.writeheader()
+        for r in csv_rows:
+            writer.writerow(r)
---
+++ settings.py
@@ -0,0 +1,12 @@
+url = 'https://api.twitter.com/2/tweets/search/recent'
+# Your account name
+user_name = 'USER_NAME'
+# Your consumer key
+consumer_key = 'CONSUMER_KEY'
+# Your consumer key secret
+consumer_key_secret = 'CONSUMER_KEY_SECRET'
+# Your access token
+access_token = 'ACCESS_TOKEN'
+# Your access token secret
+access_token_secret = 'ACCESS_TOKEN_SECRET'
+tweets_fields = 'public_metrics,non_public_metrics'
